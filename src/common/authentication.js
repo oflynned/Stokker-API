44ee9e80097aff2d3c8e9f7e6f2af3d725a66b6e
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-28 19:17:18 +0100

    add authentication common

--- src/common/authentication.js
+++ src/common/authentication.js
@@ -1,10 +1,44 @@
-import User from '../models/user/user';
-import { removeAccountPasswordMatch } from './featureFlags';
-import { isSessionValid } from '../controllers/session';
+import { validateUserHeader, verifyUserCredentials } from '../controllers/userController';
+import { checkSessionBySessionId } from '../controllers/sessionController';
 
-export const decodeAuthorization = (auth) => {
-  const rawDigest = auth.replace('Basic ', '');
-  const credentials = Buffer.from(rawDigest, 'base64')
+import Session from '../models/session';
+import User from '../models/user';
+
+const checkHeaderAuthValidity = (req) => {
+  const { authorization } = req.headers;
+  if (authorization === undefined || authorization === null || !authorization.includes('Basic')) {
+    throw new Error('bad_request');
+  }
+
+  const credentials = decodeHeaderAuthentication(authorization);
+  Object.assign(req.headers, credentials);
+  return validateUserHeader(req.headers);
+};
+
+const checkHeaderSessionValidity = async (req) => {
+  const sessionId = req.headers['x-session-id'];
+  if (sessionId === undefined || sessionId === null) {
+    throw new Error('bad_request');
+  }
+  const sessions = await Session.findActiveSessionsBySessionId(sessionId);
+  if (sessions.length === 0) {
+    throw new Error('session_not_valid');
+  }
+
+  // update the session expiry time
+  await checkSessionBySessionId(sessionId);
+  // also assign the assigned for the session to the request
+  req.user = sessions[0].user;
+};
+
+const doesAccountExist = ({ headers: { email } }) => User.findByEmail(email);
+
+const verifyAccountCredentials = ({ headers: { email, password: passwordAttempt } }) =>
+  verifyUserCredentials(email, passwordAttempt);
+
+export const decodeHeaderAuthentication = (auth) => {
+  const rawEncoding = auth.replace('Basic ', '');
+  const credentials = Buffer.from(rawEncoding, 'base64')
     .toString('ascii')
     .split(':');
   return {
@@ -14,70 +48,13 @@ export const decodeAuthorization = (auth) => {
   };
 };
 
-function manageHeaders({ headers }) {
-  const { authorization } = headers;
-  if (authorization === undefined || authorization === null || !authorization.includes('Basic')) {
-    throw new Error('bad_request');
-  }
-
-  Object.assign(headers, decodeAuthorization(authorization));
-}
-
-async function checkUserExists({ headers }) {
-  const { email } = headers;
-  return User.doesUserExist(email);
-}
-
-async function checkUserCredentials({ headers }) {
-  const { email, password: passwordAttempt } = headers;
-  return User.doesPasswordMatch(email, passwordAttempt);
-}
-
-export const checkAuthorization = async (req, res, next) => {
+// a user provides credentials in exchange for a session id
+// if a user doesn't have a valid session, a new one is created
+export const enforceRequiredUserCredentials = async (req, res, next) => {
   try {
-    await manageHeaders(req);
-    const exists = await checkUserExists(req);
-
-    let isValid;
-    if (removeAccountPasswordMatch()) {
-      isValid = true;
-    } else {
-      isValid = await checkUserCredentials(req);
-    }
-
-    if (exists && isValid) {
-      req.user = await User.findByEmail(req.headers.email);
-    } else {
-      delete req.user;
-    }
-
-    next();
-  } catch (err) {
-    res.status(400)
-      .send({ error: err.message });
-  }
-};
-
-export const enforceValidSession = async (req, res, next) => {
-  const isValid = await isSessionValid(req.headers['x-session-id']);
-  if (!isValid) {
-    return res.status(403)
-      .json({ error: 'invalid_session_id' });
-  }
-
-  next();
-};
-
-export const enforceAuthorization = async (req, res, next) => {
-  try {
-    await manageHeaders(req);
-    const exists = await checkUserExists(req);
-    const isValid = await checkUserCredentials(req);
-    if (!(exists && isValid)) {
-      throw new Error('bad_credentials');
-    }
-
-    req.user = await User.findByEmail(req.headers.email);
+    await checkHeaderAuthValidity(req);
+    await doesAccountExist(req);
+    req.user = await verifyAccountCredentials(req);
     next();
   } catch (err) {
     switch (err.message) {
@@ -90,8 +67,29 @@ export const enforceAuthorization = async (req, res, next) => {
         res.status(401)
           .send({ error: err.message });
         break;
-      case 'user_not_verified':
-        res.status(403)
+      case 'user_does_not_exist':
+        res.status(404)
+          .send({ error: err.message });
+        break;
+    }
+  }
+};
+
+// the user must provide a session id to exchange for a server-side user object
+// a session provided here should also have its TTL extended as activity has been announced
+export const enforceRequiredValidSessionId = async (req, res, next) => {
+  try {
+    await checkHeaderSessionValidity(req);
+    next();
+  } catch (err) {
+    switch (err.message) {
+      default:
+      case 'bad_request':
+        res.status(400)
+          .send({ error: err.message });
+        break;
+      case 'session_not_valid':
+        res.status(401)
           .send({ error: err.message });
         break;
       case 'user_does_not_exist':
