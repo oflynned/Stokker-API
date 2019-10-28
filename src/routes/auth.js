aedfe8fe0b7d928bf98810c439d404ea88812d77
Edmond O'Flynn <oflynned@tcd.ie>
2019-10-28 19:15:53 +0100

    add auth get req routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,20 +1,30 @@
-import { enforceRequiredUserCredentials } from '../common/authentication';
+import {
+  enforceRequiredUserCredentials,
+  enforceRequiredValidSessionId
+} from '../common/authentication';
 import { requestSessionByUser } from '../controllers/sessionController';
+import Session from '../models/session';
 
 const express = require('express');
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/health', (req, res) => {
-  res.json({ status: 'ok' });
-});
-
 router.post(
-  '/auth',
+  '/',
   enforceRequiredUserCredentials,
   async (req, res) => {
     const session = await requestSessionByUser(req.user);
     res.json(session);
+  }
+);
+
+router.get(
+  '/',
+  enforceRequiredValidSessionId,
+  async (req, res) => {
+    const session = await Session.extendSessionExpiry(req.user);
+    res.status(200)
+      .json(session);
   }
 );
 
